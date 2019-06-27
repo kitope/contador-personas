0046e99ac8ba19f713527136c4dcaef5b3fc0791
cmolina <cmolina@ewarecorp.com>
2019-06-26 23:30:47 -0400

    up

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-
+const path = require('path');
 app.get('/registros-camaras', function (req, res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(process.env.PORT || 5000, function () {
