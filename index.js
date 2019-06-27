2338d8e94b1f3484669f4ac7cd752587ae310328
cmolina <cmolina@ewarecorp.com>
2019-06-26 23:37:22 -0400

    up

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'https://www.db4free.net',
+  user     : 'cmolina',
+  password : '2019Cmp2',
+  database : 'contador'
+});
+
+
 app.get('/registros-camaras', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+        if (err) throw err;
+        console.log('The solution is: ', rows[0].solution);
+        res.sendFile(path.join(__dirname+'/index.html'));
+    });
+
+    connection.end();
 });
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port !');
+    console.log('Example app listening on port !');
 });
