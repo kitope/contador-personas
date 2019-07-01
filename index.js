89ea97d054410f6b131e2de4c8669c0c5a910a87
cmolina <cmolina@ewarecorp.com>
2019-07-01 15:05:48 -0400

    up

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-var express = require('express');
-var app = express();
-const path = require('path');
-var mysql = require('mysql');
+var express = require('express')
+var app = express()
+const path = require('path')
+var mysql = require('mysql')
 
 var connection = mysql.createConnection({
   host     : '85.10.205.173',
   user     : 'cmolina',
   password : '2019Cmp2',
   database : 'contador'
-});
+})
 
 
 app.get('/registros-camaras', function (req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
 
 app.get('/api/registros', function (req, res) {
     connection.connect();
-    connection.query('SELECT * FROM cont', function(err, rows, fields) {
-        if (err) throw err;
-        console.log('database contador  is: ', rows[0]);
+    connection.query('SELECT DATE(fecha), SUM(entrada), SUM(salida) FROM cont group by DATE(fecha)', function(err, rows, fields) {
+        if (err) throw err
         res.send(rows)
     });
-    connection.end();
-});
+    connection.end()
+})
 
 app.listen(process.env.PORT || 5000, function () {
-    console.log('Example app listening on port !');
-});
+    console.log('Example app listening on port !')
+})
